064e9ed111ea788e6892cd02be65f733aa957f0c
stevenlay <imstevencoding@gmail.com>
2021-01-21 13:12:25 -0800

    update keys

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,4 @@ module.exports = {
   spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
-  setlistKey: process.env.SETLIST_KEY,
 };
